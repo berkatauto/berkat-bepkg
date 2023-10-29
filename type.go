e698a7597e329d13e9d443b85af83483ddea2926
Rachma-Nurhaliza-Parindra <rachmanurhalizastudent@gmail.com>
2023-10-29 14:06:09 +0700

    Update (Type) : Memperbarui Type

--- type.go
+++ type.go
@@ -7,6 +7,18 @@ type Article struct {
 	Tags  string `json:"tags" bson:"tags"`
 }
 
+type User struct {
+	Username string `json:"username" bson:"username"`
+	Password string `json:"password" bson:"password"`
+	Role     string `json:"role,omitempty" bson:"role,omitempty"`
+}
+
+type Credential struct {
+	Status  bool   `json:"status" bson:"status"`
+	Token   string `json:"token,omitempty" bson:"token,omitempty"`
+	Message string `json:"message,omitempty" bson:"message,omitempty"`
+}
+
 // NewArticle adalah fungsi pembuat untuk membuat instance baru dari Article
 func NewArticle(title, tags string) *Article {
 	return &Article{
@@ -24,15 +36,7 @@ func (a *Article) ToJSON() (string, error) {
 	return string(jsonData), nil
 }
 
-
-type User struct {
-	Username string `json:"username" bson:"username"`
-	Password string `json:"password" bson:"password"`
-	Role     string `json:"role,omitempty" bson:"role,omitempty"`
-}
-
-type Credential struct {
-	Status  bool   `json:"status" bson:"status"`
-	Token   string `json:"token,omitempty" bson:"token,omitempty"`
-	Message string `json:"message,omitempty" bson:"message,omitempty"`
+// PrintInfo mencetak informasi artikel ke layar
+func (a *Article) PrintInfo() {
+	fmt.Printf("Title: %s\nTags: %s\n", a.Title, a.Tags)
 }
