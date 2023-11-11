d8de451384744d858641d8f211199152804597a7
adam-ghafara <adam.ghafara.job@gmail.com>
2023-11-11 15:00:32 +0700

    Add (GCFPostArticle) : Function Baru untuk Post Article

--- mongo.go
+++ mongo.go
@@ -21,8 +21,22 @@ func GetArticle(mongoconn *mongo.Database, collection string) []Article {
 	return tampilartikel
 }
 
+func GetNameAndPassowrd(mongoconn *mongo.Database, collection string) []User {
+	user := atdb.GetAllDoc[[]User](mongoconn, collection)
+	return user
+}
+
+func GetAllUser(mongoconn *mongo.Database, collection string) []User {
+	user := atdb.GetAllDoc[[]User](mongoconn, collection)
+	return user
+}
+
 func IsPasswordValid(mongoconn *mongo.Database, collection string, userdata User) bool {
 	filter := bson.M{"username": userdata.Username}
 	res := atdb.GetOneDoc[User](mongoconn, collection, filter)
 	return CheckPasswordHash(userdata.Password, res.Password)
+}
+
+func PostArticle(mongoconn *mongo.Database, collection string, Article) interface{} {
+	return atdb.InsertOneDoc(mongoconn, collection, articleData)
 }
--- peda.go
+++ peda.go
@@ -2,10 +2,13 @@ package berkatbepkg
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"os"
 
+	"github.com/aiteung/atdb"
 	"github.com/whatsauth/watoken"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 func GCFHandler(MONGOCONNSTRINGENV, dbname, collectionname string) string {
@@ -40,27 +43,57 @@ func GCFPostHandler(PASETOPRIVATEKEYENV, MONGOCONNSTRINGENV, dbname, collectionn
 	return GCFReturnStruct(Response)
 }
 
+func GCFCreateUser(MONGOCONNSTRINGENV, dbname, collectionname string, r *http.Request) string {
+	mconn := SetConnection(MONGOCONNSTRINGENV, dbname)
+	var datauser User
+	err := json.NewDecoder(r.Body).Decode(&datauser)
+	if err != nil {
+		return err.Error()
+	}
+
+	// Hash the password before storing it
+	hashedPassword, hashErr := HashPassword(datauser.Password)
+	if hashErr != nil {
+		return hashErr.Error()
+	}
+	datauser.Password = hashedPassword
+
+	createErr := CreateNewUserRole(mconn, collectionname, datauser)
+	fmt.Println(createErr)
+
+	return GCFReturnStruct(datauser)
+}
+
+func CreateNewUserRole(mongoconn *mongo.Database, collection string, userdata User) interface{} {
+	// Hash the password before storing it
+	hashedPassword, err := HashPassword(userdata.Password)
+	if err != nil {
+		return err
+	}
+	userdata.Password = hashedPassword
+
+	// Insert the user data into the database
+	return atdb.InsertOneDoc(mongoconn, collection, userdata)
+}
+
 func GCFReturnStruct(DataStuct any) string {
 	jsondata, _ := json.Marshal(DataStuct)
 	return string(jsondata)
 }
 
-// func CreateUser(MONGOCONNSTRINGENV, dbname, collectionname string, r *http.Request) string {
-// 	var response Credential
-// 	response.Status = false
-// 	mconn := SetConnection(MONGOCONNSTRINGENV, dbname)
-// 	var datauser User
-// 	err := json.NewDecoder(r.Body).Decode(&datauser)
-// 	if err != nil {
-// 		response.Message = "error parsing application/json: " + err.Error()
-// 	} else {
-// 		response.Status = true
-// 		hash, hashErr := HashPassword(datauser.Password)
-// 		if hashErr != nil {
-// 			response.Message = "Gagal Hash Password" + err.Error()
-// 		}
-// 		(mconn, collectionname, datauser.Username, datauser.Role, hash)
-// 		response.Message = "Berhasil Input data"
-// 	}
-// 	return GCFReturnStruct(response)
-// }
+func GCFPostArticle(MONGOCONNSTRINGENV, dbname, collectionname string, r *http.Request) string {
+	mconn := SetConnection(MONGOCONNSTRINGENV, dbname)
+	var newarticle Article
+	err := json.NewDecoder(r.Body).Decode(&newarticle)
+	if err != nil {
+		return err.Error()
+	}
+
+	// Get PASETO Header Value
+	// pasetoValue := r.Header.Set("PASETOPRIVATEKEYENV")
+	// Post The Article
+	response := GCFReturnStruct(newarticle)
+	// response += "PASETO Value: " + pasetoValue
+	PostArticle(mconn, collectionname, newarticle)
+	return response
+}
